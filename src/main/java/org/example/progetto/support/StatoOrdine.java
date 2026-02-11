04accdc7280a557c8dd0522958f50af0d22e9afe
paolo <paolosicoli07@gmail.com>
2026-02-11 13:55:56 +0100

    Correzioni

---
+++ src/main/java/org/example/progetto/support/StatoOrdine.java
@@ -0,0 +1,11 @@
+package org.example.progetto.support;
+
+public enum StatoOrdine {
+    IN_ATTESA_DI_PAGAMENTO,
+    PAGAMENTO_COMPLETATO,
+    PAGAMENTO_FALLITO,
+    IN_PREPARAZIONE,
+    SPEDITO,
+    CONSEGNATO,
+    ANNULLATO
+}
